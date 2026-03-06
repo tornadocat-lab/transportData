f24801336daa37b7c50b247bab50aa73054c8614
tornadocat <tornadocat106@gmail.com>
2026-03-06 16:42:38 +0800

    add frontend, query for shift, add new table'stations'.

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import query
+
+app = FastAPI()
+
+# CORS for frontend testing
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/stations")
+def get_stations():
+    return query.get_stations()
+
+@app.get("/trains")
+def get_trains(from_station: str = Query(..., alias="from"), to_station: str = Query(..., alias="to")):
+    return query.get_trains_between(from_station, to_station)
